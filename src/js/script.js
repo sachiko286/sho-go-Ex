2f0a71ecbe972362021b4907b6a1143424eb294b
sachiko.t <sattsuttsu66-1@yahoo.co.jp>
2023-12-27 01:01:13 +0900

    20231227

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,14 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $(window).on("load", function () {
         $(".js-loading__img").delay(800).fadeOut(600); // 画像をフェードアウト
         $(".js-loading").delay(1000).fadeOut(600); // 背景色をフェードアウト
-        });
-        // ページの読み込みが完了しなくても2秒経ったら強制的にローディング画面をフェードアウト
-        setTimeout(stoploading, 2000);
-        function stoploading() {
+    });
+    // ページの読み込みが完了しなくても2秒経ったら強制的にローディング画面をフェードアウト
+    setTimeout(stoploading, 2000);
+    function stoploading() {
         $(".js-loading").fadeOut(600);
-        }
+    }
 
-        // ハンバーガーメニュー
+    // ハンバーガーメニュー
     $(function () {
         $(".js-hamburger").on("click", function () {
             $(this).toggleClass("is-open");
@@ -29,7 +29,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
 
         // resizeイベント
-        $(window).on('resize', function() {
+        $(window).on('resize', function () {
             if (window.matchMedia("(min-width: 768px)").matches) {
                 closeDrawer();
             }
@@ -42,7 +42,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     }
 
     function closeDrawer() {
-            $(".js-drawer").fadeOut();
+        $(".js-drawer").fadeOut();
         $(".js-hamburger").removeClass("is-open");
     }
 
@@ -73,11 +73,11 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 
     // サインインボタン非活性
     $(document).ready(function () {
-        var $submitBtn = $('#js-submit');
-        $('.form input').on('change', function () {
+        var $submitBtn = $('#js-modal-submit');
+        $('.modal__form input').on('change', function () {
             if (
-                $('.form input[type="email"]').val() !== "" && 
-                $('.form input[type="password"]').val() !== ""
+                $('.modal__form input[type="email"]').val() !== "" &&
+                $('.modal__form input[type="password"]').val() !== ""
             ) {
                 $submitBtn.prop('disabled', false);
                 $submitBtn.addClass('modal__submit-active');
@@ -87,6 +87,55 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
             }
         });
     });
-        
+
+    // 送信ボタン非活性
+    $(document).ready(function () {
+        var $submitBtn = $('#js-entry__submit');
+            $('.entry__form input, .entry__form textarea').on('change', function () {
+            
+            if (
+                $('.entry__form input[type="radio"]:checked').length !== 0 && // チェックの数が０ではない
+                $('.entry__form #name').val() !== "" && 
+                $('.entry__form #age').val() !== "" && 
+                $('.entry__form #mail').val() !== "" &&
+                $('.entry__form #income').val() !== "" &&
+                $('.entry__form textarea[name="resume"]').val() !== "" &&
+                $('.entry__form textarea[name="pr"]').val() !== "" &&
+                $('.entry__form #privacyCheck').prop('checked') === true
+            ) {
+                $submitBtn.prop('disabled', false);
+                $submitBtn.addClass('form-submit-active');
+            } else {
+                $submitBtn.prop('disabled', true);
+                $submitBtn.removeClass('form-submit-active');
+            }
+            });
+        });
+    
+    // サンクスページへ
+    $(document).ready(function () {
+        $('.entry__form').submit(function (event) {
+            // var formData = $('#form').serialize();
+            // $.ajax({
+            // url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdEPrh7Faf6xDivpTrRPNQhix3T0ndMkKZD7H-g1AkyMxhnqQ/formResponse",
+            // data: formData,
+            // type: "POST",
+            // dataType: "xml",
+            // statusCode: {
+            // 0: function () {
+            // //$(".end-message").slideDown();
+            // //$(".submit-btn").fadeOut();
+            window.location.href = "thanks.html";
+            // },
+            // 200: function () {
+            // $(".false-message").slideDown();
+            // }
+            // }
+            // });
+            event.preventDefault();
+        });
+    });
+
+
 
 });
